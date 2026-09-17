c372824510062355dae5fa2ea6cdad91097d66df
bmercado25 <breemercado19@gmail.com>
2026-09-17 19:11:14 -0400

    name change

--- BTS mansion game web port/src/userInterface.ts
+++ BTS mansion game web port/src/userInterface.ts
@@ -68,7 +68,7 @@ export class UserInterface {
     this.terminal.setStatus({ room: "MENU", sanity: null });
     this.terminal.setPlaceholder("");
     this.suppressNoticeCapture = true;
-    this.terminal.print("Mansion", "heading");
+    this.terminal.print("malum", "heading");
 
     this.terminal.print("===== Main Menu =====", "system");
     this.terminal.print("START Game", "exits");
